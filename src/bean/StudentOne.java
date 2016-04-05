e5a45a91c006414b46f1357df0b97a58173e654b
Temo <temo.kiknavelidze@gmail.com>
2016-04-05 04:41:58 +0400

    Initial commit

---
+++ src/bean/StudentOne.java
@@ -0,0 +1,39 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+@SessionScoped
+@ManagedBean
+public class StudentOne implements Serializable{
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public StudentOne() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
