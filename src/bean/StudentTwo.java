d03087eb569711a53755312661cd6b47face3c66
Temo <temo.kiknavelidze@gmail.com>
2016-04-05 05:23:29 +0400

    ...validation error on single line(next to input form) with length and range check...

---
+++ src/bean/StudentTwo.java
@@ -0,0 +1,58 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@SessionScoped
+@ManagedBean
+public class StudentTwo implements Serializable{
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int freePasses;
+    private int postalCode;
+
+    public StudentTwo() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+}
